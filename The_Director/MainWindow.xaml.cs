94c398c71f62b2c7ac8bad444f7b56228c826221
ty <2305597132@qq.com>
2023-01-30 01:49:43 +0800

    添加项目文件。

---
+++ The_Director/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace The_Director
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Reselected(object sender, RoutedEventArgs e)
+        {
+            switch(Menu.SelectedIndex)
+            {
+                case 1:
+                    this.WindowFrame.Source = new Uri("/Windows/SettingsPage.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                case 2:
+                    this.WindowFrame.Source = new Uri("/Windows/AboutPage.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                default:
+                    this.WindowFrame.Source = null;
+                    break;
+            }
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ The_Director/Windows/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace The_Director.Windows
+{
+    /// <summary>
+    /// AboutPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class AboutPage : UserControl
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
